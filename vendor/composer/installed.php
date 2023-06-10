2f70d11d12b24f497a2cffa2919ecdbada8df8b3
17607115508 <wekyun@qq.com>
2023-06-10 22:16:23 +0800

    cs

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '5f1638fb4897d258d21dfcf33febe7a399ef8a81',
+        'name' => 'wekyun/webman_tool',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wekyun/webman_tool' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '5f1638fb4897d258d21dfcf33febe7a399ef8a81',
+            'dev_requirement' => false,
+        ),
+    ),
+);
